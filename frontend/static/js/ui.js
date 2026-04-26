8d7880cd2de0e6908fd4460be73d87b28fdd5360
MichalADA <michal@LLWAPF3TJ7MB.onwelo.net>
2026-04-26 15:28:46 +0200

    Dodanie avatarow

--- frontend/static/js/ui.js
+++ frontend/static/js/ui.js
@@ -19,6 +19,10 @@ const AVAILABLE_AVATARS = [
   'avatar-1.png',
   'avatar-2.png',
   'avatar-3.png',
+  'avatar-4.png',
+  'avatar-5.png',
+  'avatar-6.png',
+
 ];
 
 // Heurystyka: ciąg znaków wyglądający jak nazwa pliku obrazka.
